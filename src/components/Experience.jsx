dca0297c510239d24da415b84e5afde10086761f
Muzaffer Talha Candan <muzaffertcandan@gmail.com>
2023-09-09 03:32:40 +0300

    half of it

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,17 @@
 import { OrbitControls } from "@react-three/drei";
-
+import { Avatar } from "./Avatar";
+// console.log(Avatar);
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group  position-y={-1}>
+        <Avatar />
+      </group>
+      {/* <MyAvatar/> */}
+      {/* <Human/> */}
+      {/* <Model/> */}
+      <ambientLight intensity={1} />
     </>
   );
 };
